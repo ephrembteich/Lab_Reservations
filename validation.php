710c90d9b4d747b518837968cd4aef349a6f79e3
kkareem27 <kkareem27@gmail.com>
2015-12-20 21:56:01 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,18 +1,40 @@
 <?php
 
-/*** first check that both the username, password have been sent ***/
-if(!isset( $_POST['name'], $_POST['password']))
-{
-    $message = 'Please enter a valid username and password';
-}
-else
-{
-
-//    $name = 'kjk03';
-//    $password = 'kareemisawesome';
     /*** if we are here the data is valid and we can insert it into database ***/
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+
+/***    $lab = intval($_POST['lab']);
+    $day = intval($_POST['day']);
+    $month = intval($_POST['month']);
+    $year = intval($_POST['year']);
+***/
+
+    $day = 20;
+    $month = 12;
+    $year = 2015;
+
+    $days = 0;
+
+    $date = $year.$month.$day;
+    $dateTime = strtotime("+".$days." days", strtotime($date));
+    $date = date("Y-m-d", $dateTime);
+
+    $dayOfWeek = date("N", mktime(0, 0, 0, $month, $day, $year));
+    print_r($dayOfWeek);
+    print_r('<br>');
+    if ($dayOfWeek != 1){
+        $mondayOfWeekTime = strtotime("last Monday", $dateTime);
+        $mondayOfWeek = date("Y-m-d", $mondayOfWeekTime);
+    }
+    else {
+        $mondayOfWeek = $date;
+    }
+//    print("Today is ".$date);
+//    print("<br>");
+//    print_r($dayOfWeek);
+//    print_r("<br>");
+//    print("Last monday was ".$mondayOfWeek);
+//    print("<br>");
+//    print("<br>");
 
     /*** connect to database ***/
     /*** mysql hostname ***/
@@ -22,13 +44,13 @@ else
     $mysql_username = 'root';
 
     /*** mysql password ***/
-    $mysql_password = 'Kkareem_27';
+    $mysql_password = 'admin';
 
     /*** database name ***/
     $mysql_dbname = 'lab_reservation';
 
     /*** table name ***/
-    $mysql_table = 'account';
+    $mysql_table = 'eventcalendar';
 
 
     $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
@@ -36,40 +58,72 @@ else
     /*** set the error mode to excptions ***/
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    /*** check for user ***/
-    $result = $dbh->query("SELECT *
+
+    /*** check for reservations ***/
+    $result = $dbh->query("SELECT Title, Lab, eventDate, eventStart, eventLength, addedBy
                             FROM $mysql_table
-                            WHERE username = '".$name."'");
+                            WHERE eventDate >= '".$mondayOfWeek."'
+                            AND eventDate <= date_add('".$mondayOfWeek."', INTERVAL 6 DAY)
+                            ORDER BY eventDate ASC");
+    $result->execute();
+    $result = $result->fetchAll(PDO::FETCH_ASSOC);
+//    print_r($result);
+//    print_r('<br>');
+    foreach ($result as $key => &$value) {
+//        print_r($value['eventDate']);
+ //       print_r('<br>');
+        list($year, $month, $day) = explode('-', $value['eventDate']);
+        $dayOfWeek = date("l", mktime(0, 0, 0, $month, $day, $year));
 
-    if ($result->rowCount() > 0) {
-    //User exists
-    //check password
-        $result->execute();
-        $result = $result->fetchAll();
+        $value = array('dayOfWeek' => $dayOfWeek)+$value;
+        //$value['dayOfWeek'] = $dayOfWeek;
+//        print_r($dayOfWeek);
 
-        if($result[0]['password'] == $password){
-            print_r("true");
-            return "true";
-        }
-        else {
-            print_r("pass");
-            return "pass";
-        }
+//    print_r('<br>');
     }
-    else {
-        print_r("user");
-        return "user";
+ //   print_r($result);
+
+//    print_r("<br>");
+//    print_r("<br>");
+//    print_r("<br>");
+    $result = json_encode($result);
+    print_r($result);
+
+    return $result;
+
+
+
+
+
+/***    if (count($result) > 0) {?>
+    <table border="2">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Lab</th>
+          <th>Date of Reservation</th>
+          <th>Time</th>
+          <th>Duration</th>
+        </tr>
+      </thead>
+        <?php
+          foreach ($result as $key => $value) { ?>
+          <tbody>
+            <td><?=$value['Title']?></td>
+            <td><?=$value['Lab']?></td>
+            <td><?=$value['eventDate']?></td>
+            <td><?=$value['eventStart']?></td>
+            <td><?=$value['eventLength']?></td>
+          </tbody>
+          <?php }
+        ?>
+
+    </table>
+
+    <?php
     }
+    else{
+        print("No reservations for the week of $date");
+    }  ***/
+    ?>
 
-//}
-?>
-
-<html>
-    <head>
-        <title>Login validation</title>
-    </head>
-
-    <body>
-        <p><?php echo $message; ?>
-    </body>
-</html>
