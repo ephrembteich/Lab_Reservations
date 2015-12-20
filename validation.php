229df5f49370901e1b2c5389f9a2637cc931f70d
Ephrem Bteich <ehb03@mail.aub.edu>
2015-12-20 23:08:24 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,10 +1,11 @@
 <?php
-
-    $name = 'kjk03';
-    $password = 'kareemisawesome';
+    session_start();
+    
+    //$name = 'kjk03';
+    //$password = 'kareemisawesome';
     /*** if we are here the data is valid and we can insert it into database ***/
-//    $name = $_POST['name'];
-//    $password = $_POST['password'];
+    $name = $_POST['name'];
+    $password = $_POST['password'];
 
     /*** connect to database ***/
     /*** mysql hostname ***/
@@ -15,6 +16,7 @@
 
     /*** mysql password ***/
     $mysql_password = 'admin';
+    //$mysql_password = 'mypass';
 
     /*** database name ***/
     $mysql_dbname = 'lab_reservation';
@@ -24,6 +26,8 @@
 
 
     $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+    
+    $name = $dbh->quote($name);
 
     /*** set the error mode to excptions ***/
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -40,13 +44,15 @@
         $result = $result->fetchAll();
 
         if($result[0]['password'] == $password){
-            return "true";
+            //if the credentials are correct then start a session with the users name
+			$_SESSION["user"]=$_POST['name'];
+            echo "true";
         }
         else {
-            return "pass";
+            echo "pass";
         }
     }
     else {
-        return "user";
+        echo "user";
     }
 ?>
