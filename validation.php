96d06004a8d027db5877216092b7c3d98de9e4e2
Ephrem Bteich <ehb03@mail.aub.edu>
2015-12-21 19:15:58 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,41 +1,38 @@
 <?php
-    session_start();
-    
-    //$name = 'kjk03';
+	session_start();
+	
+	//$name = 'kjk03';
     //$password = 'kareemisawesome';
-    /*** if we are here the data is valid and we can insert it into database ***/
-    $name = $_POST['name'];
-    $password = $_POST['password'];
+	
+	$name = $_POST['name'];
+	$password = $_POST['password'];
 
-    /*** connect to database ***/
+	/*** connect to database ***/
     /*** mysql hostname ***/
     $mysql_hostname = 'localhost';
-
     /*** mysql username ***/
     $mysql_username = 'root';
-
     /*** mysql password ***/
-    $mysql_password = 'admin';
-    //$mysql_password = 'mypass';
-
+	$mysql_password = 'mypass';
+    //$mysql_password = 'admin';
+	
     /*** database name ***/
     $mysql_dbname = 'lab_reservation';
-
     /*** table name ***/
     $mysql_table = 'account';
 
-
-    $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
-    
-    $name = $dbh->quote($name);
-
-    /*** set the error mode to excptions ***/
+	$dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+	
+	/*** set the error mode to excptions ***/
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+	
+	//to encode special characters
+	$name = $dbh->quote($name);
+	
     /*** check for user ***/
     $result = $dbh->query("SELECT *
                             FROM $mysql_table
-                            WHERE username = '".$name."'");
+                            WHERE username = ".$name);
 
     if ($result->rowCount() > 0) {
     //User exists
@@ -44,15 +41,15 @@
         $result = $result->fetchAll();
 
         if($result[0]['password'] == $password){
-            //if the credentials are correct then start a session with the users name
+			//if the credentials are correct then start a session with the users name
 			$_SESSION["user"]=$_POST['name'];
-            echo "true";
+            echo("true");
         }
         else {
-            echo "pass";
+            echo("pass");
         }
     }
     else {
-        echo "user";
+        echo("user");
     }
 ?>
